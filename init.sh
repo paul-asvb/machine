251cb7a04d0c060ccaabe0ba549ee5d430a6bd3e
paul-asvb <4126256+paul-asvb@users.noreply.github.com>
2024-03-21 21:04:34 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,39 @@
+
+sudo pacman -Syu --noconfirm base base-devel protobuf unzip lld lcov
+sudo pacman -Syu --noconfirm postgresql-libs aws-cli typescript 
+sudo pacman -Syu --noconfirm fzf helm kubectl zsh cmake ccls jq yq musl neovim 
+sudo pacman -Syu --noconfirm ripgrep htop curl wget git vim docker openssh bridge-utils pass avahi llvm
+sudo pacman -Syu --noconfirm tmate clang linux-tools gnu-netcat tmate aarch64-linux-gnu-gcc brightnessctl bat neomutt kitty aspell-uk aspell-en aspell-de yubikey-manager yubico-pam freerdp simple-scan docker-compose
+sudo pacman -Syu --noconfirm vlc chromium ttf-font-awesome awesome-terminal-fonts otf-font-awesome adapta-gtk-theme papirus-icon-theme go dnsmasq openssh extra/noto-fonts extra/noto-fonts-emoji tmux terraform
+sudo usermod -aG docker $(whoami)
+sudo systemctl enable docker
+
+# Rust Development
+sudo pacman -S rustup --noconfirm
+rustup toolchain add stable
+rustup toolchain add nightly
+for target in "wasm32-wasi" "wasm32-unknown-unknown" "x86_64-unknown-linux-musl" "aarch64-unknown-linux-gnu" "aarch64-unknown-linux-musl"; do
+	rustup target add $target
+done
+
+rustup component add rls rust-analysis rust-src clippy
+rustup component add llvm-tools-preview --toolchain nightly
+rustup component add llvm-tools-preview
+
+cargo install cargo-audit --features=fix
+cargo install git-cliff
+cargo install cargo2junit
+cargo install cargo-make
+cargo install grcov
+cargo install sqlx-cli --no-default-features --features native-tls,postgres
+cargo install cargo-nextest --version 0.9.39
+cargo install cargo-about
+cargo install cargo-license
+cargo install --locked trunk
+cargo install --locked gitlab-report
+
+sudo pacman -Syu --noconfirm  rust-analyzer
+
 yay fzf alacritty
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
