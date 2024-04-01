b68d1f1da8a64139dae2971e2e95d9e91f10f3b2
paul-asvb <4126256+paul-asvb@users.noreply.github.com>
2024-04-01 10:51:44 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -34,9 +34,9 @@ cargo install --locked gitlab-report
 
 sudo pacman -Syu --noconfirm  rust-analyzer
 
-yay fzf alacritty
+yay fzf alacritty codium
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/ohmyzsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  $HOME/.config/zsh/ohmyzsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git  $HOME/.config/zsh/ohmyzsh/plugins/plugins/fast-syntax-highlighting
+git clone https://github.com/marlonrichert/zsh-autocomplete.git  $HOME/.config/zsh/ohmyzsh/plugins/plugins/zsh-autocomplete
