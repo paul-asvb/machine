5c9ce29042b2040efbd77645ba542519d0cb659c
paul-asvb <4126256+paul-asvb@users.noreply.github.com>
2024-09-12 11:14:08 +0200

    Update syncthing.sh

--- syncthing.sh
+++ syncthing.sh
@@ -1,6 +1,5 @@
 mkdir -p /home/$USER/.config/systemd/user/
-cp ./syncthing.service /home/$USER/.config/systemd/user/syncthing.service
-
+curl https://raw.githubusercontent.com/paul-asvb/machine/main/syncthing.service -o  /home/$USER/.config/systemd/user/syncthing.service
 systemctl enable --user syncthing.service
 systemctl start --user syncthing.service
 systemctl status --user syncthing.service
