47e734e7eec1fa3a5192e70bcccdfbbc13c31a97
Paul Mayer <paul@paul-mayer.eu>
2026-06-11 08:49:39 +0200

    feat: init waybar

--- hyprinit.sh
+++ hyprinit.sh
@@ -4,7 +4,7 @@
 
 # Hyprland + the wayland utilities that map onto the sway config
 sudo pacman -Syu --noconfirm hyprland hypridle hyprlock hyprpaper
-sudo pacman -Syu --noconfirm brightnessctl alacritty
+sudo pacman -Syu --noconfirm brightnessctl alacritty wofi waybar
 # build deps required by hyprpm to compile hy3 against the running Hyprland
 sudo pacman -Syu --noconfirm base-devel cmake cpio meson git
 
